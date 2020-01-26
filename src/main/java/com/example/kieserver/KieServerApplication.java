a05822e059e43017513b5f47c7b2488186e68b24
serzhu <1083511@gmail.com>
2020-01-26 22:35:50 +0300

    initial commit;

---
+++ src/main/java/com/example/kieserver/KieServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.kieserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KieServerApplication.class, args);
+    }
+
+}
